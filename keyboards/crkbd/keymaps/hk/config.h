084da4620e9e0283e4102ba3d03c580d1d2bed1f
Jonas Ha <jonas-ha@outlook.com>
2026-04-25 19:06:09 -0400

    Fix ps2 pointing device driver build on vial-qmk base
    Port ps2 driver support from allie-cat: add ps2 to valid driver types,
    wire PS2_ENABLE when POINTING_DEVICE_DRIVER=ps2, add ps2.h include to
    pointing_device.h, copy drivers/sensors/ps2.c/.h, and add required
    VIAL_KEYBOARD_UID/unlock combo defines to hk keymap config.

--- keyboards/crkbd/keymaps/hk/config.h
+++ keyboards/crkbd/keymaps/hk/config.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #define MASTER_RIGHT
+
+#define VIAL_KEYBOARD_UID {0xEA, 0x46, 0x6B, 0x1C, 0x6F, 0xAF, 0x64, 0x13}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
